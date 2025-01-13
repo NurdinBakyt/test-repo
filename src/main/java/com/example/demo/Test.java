6a3a0cfbf2467f28f039e6c28249d0b0f1d36bfe
Нурдин Бакытбеков <bakytbekovnurdin7@gmail.com>
2025-01-13 19:22:43 +0600

    с ноута

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -2,12 +2,28 @@ package com.example.demo;
 
 public class Test {
     private String name;
-
+    private int age;
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
